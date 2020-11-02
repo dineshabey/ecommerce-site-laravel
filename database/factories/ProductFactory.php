cbbeab20acc91dd1d09ab3237718f50187af8a9f
gitdinesh <dinabeysinghe@gmail.com>
2020-11-02 08:04:36 +0530

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,11 @@
+<?php
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'product_name' => $faker->sentence(2),
+        'description'  => $faker->sentence(20),
+        'price'        => $faker->numberBetween(100,5000),
+    ];
+});
---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        factory(App\Product::class, 100)->create();
+       
+    }
+}
